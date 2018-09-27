c3c91b4b00e9dc322fc183ceae653a8774034f8b
Qu1oX <danny1996@gmx.net>
2018-09-27 11:25:00 +0200

    First commit

---
+++ js/onClickListeners.js
@@ -0,0 +1,15 @@
+function startButtonClicked()
+{
+    var text=document.getElementById('startButton').innerText;
+    console.log(text);
+}
+
+function optionButtonClicked() {
+    var text=document.getElementById('optionButton').innerText;
+    console.log(text);
+}
+
+function creditsButtonClicked() {
+    var text=document.getElementById('creditsButton').innerText;
+    console.log(text);
+}
