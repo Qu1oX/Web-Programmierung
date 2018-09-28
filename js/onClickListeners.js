22beaa83c4b6d24d68a8dfcaa35a4b4c90cc1627
Voxen4 <m.keller490@gmail.com>
2018-09-28 21:08:28 +0200

    Removed ClickListeners From index.html, Added Key Listener to Game Window

--- js/onClickListeners.js
+++ js/onClickListeners.js
@@ -1,11 +1,38 @@
-function startButtonClicked()
-{
-    var text=document.getElementById('startButton').innerText;
-    console.log(text);
+function initListener() {
+    //https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
+    window.addEventListener("keydown", function (event) {
+        if (event.defaultPrevented) {
+            return; // Do nothing if the event was already processed
+        }
 
-}
+        switch (event.key) {
+            case "Down": // IE specific value
+            case "ArrowDown":
+                // Do something for "down arrow" key press.
+                break;
+            case "Up": // IE specific value
+            case "ArrowUp":
+                // Do something for "up arrow" key press.
+                break;
+            case "Left": // IE specific value
+            case "ArrowLeft":
+                // Do something for "left arrow" key press.
+                break;
+            case "Right": // IE specific value
+            case "ArrowRight":
+                // Do something for "right arrow" key press.
+                break;
+            case "Enter":
+                // Do something for "enter" or "return" key press.
+                break;
+            case "Escape":
+                // Do something for "esc" key press.
+                break;
+            default:
+                return; // Quit when this doesn't handle the key event.
+        }
 
-function optionButtonClicked() {
-    var text=document.getElementById('optionButton').innerText;
-    console.log(text);
+        // Cancel the default action to avoid it being handled twice
+        event.preventDefault();
+    }, true);
 }
